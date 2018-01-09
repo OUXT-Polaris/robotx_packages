bb0deee8a7b8b7180ae55e7a6288fadfa9822066
masaya <ms.kataoka@gmail.com>
2018-01-09 23:07:40 +0900

    add get_pcl_pointcloud and generate_voxel_chain_input functions to voxelchain_input_generator class

--- robotx_recognition/include/voxelchain_input_generator.h
+++ robotx_recognition/include/voxelchain_input_generator.h
@@ -3,9 +3,17 @@
 
 //headers in this package
 #include <robotx_msgs/EuclideanClusters.h>
+#include <robotx_msgs/VoxelChainInputs.h>
 
 //headers in ROS
 #include <ros/ros.h>
+
+//headers in pcl
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/console/time.h>
+#include <pcl/filters/voxel_grid.h>
 
 class voxelchain_input_generator
 {
@@ -14,6 +22,8 @@ public:
   ~voxelchain_input_generator();
   void euclidean_clusters_callback(robotx_msgs::EuclideanClusters msg);
 private:
+  robotx_msgs::VoxelChainInput generate_voxel_chain_input(sensor_msgs::PointCloud2 cluster_pointcloud);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr get_pcl_pointcloud(sensor_msgs::PointCloud2 pointcloud_msg);
   ros::Subscriber euclidean_clusters_sub_;
   ros::NodeHandle nh_;
 };
--- robotx_recognition/src/voxelchain_input_generator.cpp
+++ robotx_recognition/src/voxelchain_input_generator.cpp
@@ -15,5 +15,25 @@ voxelchain_input_generator::~voxelchain_input_generator()
 
 void voxelchain_input_generator::euclidean_clusters_callback(robotx_msgs::EuclideanClusters msg)
 {
+  robotx_msgs::VoxelChainInputs voxel_chain_inputs;
+  for(auto clusterd_pointcloud_ptr = msg.clusters.begin(); clusterd_pointcloud_ptr != msg.clusters.end(); ++clusterd_pointcloud_ptr)
+  {
+    voxel_chain_inputs.inputs.push_back(generate_voxel_chain_input(*clusterd_pointcloud_ptr));
+  }
+}
 
+robotx_msgs::VoxelChainInput voxelchain_input_generator::generate_voxel_chain_input(sensor_msgs::PointCloud2 cluster_pointcloud)
+{
+  robotx_msgs::VoxelChainInput voxel_chain_input;
+  pcl::PointCloud<pcl::PointXYZ>::Ptr target_pointcloud = get_pcl_pointcloud(cluster_pointcloud);
+  return voxel_chain_input;
+}
+
+pcl::PointCloud<pcl::PointXYZ>::Ptr voxelchain_input_generator::get_pcl_pointcloud(sensor_msgs::PointCloud2 pointcloud_msg)
+{
+  pcl::PCLPointCloud2 pcl_pc2;
+  pcl_conversions::toPCL(pointcloud_msg,pcl_pc2);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_pointcloud(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::fromPCLPointCloud2(pcl_pc2,*pcl_pointcloud);
+  return pcl_pointcloud;
 }
