df41f250766b355029d140f44a0358963f24d826
Masaya Kataoka <ms.kataoka@gmail.com>
2018-06-04 07:59:10 +0900

    add random_sampling class

---
+++ robotx_recognition/src/random_sampling.cpp
@@ -0,0 +1,46 @@
+#include <random_sampling.h>
+#include <random>
+
+random_sampling::random_sampling()
+{
+    params_ = parameters();
+}
+
+random_sampling::random_sampling(parameters params)
+{
+    params_ = params;
+}
+
+random_sampling::~random_sampling()
+{
+    
+}
+
+std::vector<cv::Rect> random_sampling::get_rois()
+{
+    std::vector<cv::Rect> rects(params_.num_roi);
+    std::random_device rd;
+    std::mt19937 mt(rd());
+    std::uniform_int_distribution<int> x_rand;
+    std::uniform_int_distribution<int> y_rand;
+    x_rand = std::uniform_int_distribution<int>(0,params_.image_width-params_.roi_width.min);
+    y_rand = std::uniform_int_distribution<int>(0,params_.image_height-params_.roi_height.min);
+    std::uniform_int_distribution<int> w_rand;
+    std::uniform_int_distribution<int> h_rand;
+    w_rand = std::uniform_int_distribution<int>(params_.roi_width.min,params_.roi_width.max);
+    h_rand = std::uniform_int_distribution<int>(params_.roi_height.min,params_.roi_height.max);
+    for(int i=0; i< rects.size(); i++)
+    {
+        int x = x_rand(mt);
+        int y = y_rand(mt);
+        int w = w_rand(mt);
+        int h = h_rand(mt);
+        if(x+w>params_.image_width)
+            w = params_.image_width - x;
+        if(y+h>params_.image_width)
+            h = params_.image_height - y;
+        cv::Rect rect{x,y,w,h};
+        rects.push_back(rect);
+    }
+    return rects;
+}
