babbc9f06427293fac8496f461e26a65711942cd
Masaya Kataoka <ms.kataoka@gmail.com>
2018-06-11 21:25:25 +0900

    add callback functions

--- robotx_driver/include/robotx_hardware_interface.h
+++ robotx_driver/include/robotx_hardware_interface.h
@@ -2,6 +2,8 @@
 #define ROBOTX_HARDWARE_INTERFACE_H_INCLUDEDE
 
 #include <ros/ros.h>
+#include <sensor_msgs/Joy.h>
+#include <std_msgs/Float32MultiArray.h>
 
 class robotx_hardware_interface
 {
@@ -19,10 +21,15 @@ public:
          */
         int mode;
         /**
-         * @brief connection timeout [sec]
+         * @brief connection timeout [sec] with motor controller
          * 
          */
         int timeout;
+        /**
+         * @brief connection timeout [sec] with joystick controller
+         * 
+         */
+        int controller_timeout;
         /**
          * @brief controller type
          * @sa robotx_hardware_interface::controllers_
@@ -42,21 +49,33 @@ public:
             ros::param::param<int>(ros::this_node::getName()+"/mode", target, all);
             ros::param::param<int>(ros::this_node::getName()+"/mode", mode, manual);
             ros::param::param<int>(ros::this_node::getName()+"/timeout", timeout, 30);
-            ros::param::param<int>(ros::this_node::getName()+"/controller", controller, dualshock4);
+            ros::param::param<int>(ros::this_node::getName()+"/controller_timeout", controller_timeout, 30);
             ros::param::param<int>(ros::this_node::getName()+"/frequency", frequency, 30);
         };
     };
     robotx_hardware_interface();
     ~robotx_hardware_interface();
 private:
+    /**
+     * @brief ROS callback function for joystick
+     * 
+     * @param msg joystick command.
+     */
+    void joy_callback_(sensor_msgs::Joy msg);
+    /**
+     * @brief ROS callback function for motor command
+     * 
+     * @param msg [left_thruster_cmd left_thruster_joint_angle right_thruster_cmd right_thruster_joint_angle]
+     */
+    void motor_command_callback_(std_msgs::Float32MultiArray msg);
     ros::NodeHandle nh_;
-    ros::Subscriber left_thrust_power_sub_;
-    ros::Subscriber left_thrust_joint_sub_;
-    ros::Subscriber right_thrust_power_sub_;
-    ros::Subscriber right_thrust_joint_sub_;
+    ros::Subscriber motor_command_sub_;
     ros::Publisher usv_drive_cmd_pub_;
     ros::Publisher left_thrust_joint_pub_;
     ros::Publisher right_thrust_joint_pub_;
+    ros::Duration last_connection_left_motor_;
+    ros::Duration last_connection_right_motor_;
+    ros::Duration last_connection_controller_;
     const parameters params_;
     enum targets_{all=0,simulation=1,hardware=2};
     enum modes_{emergency=-1,manual=0,autopilot=1};
