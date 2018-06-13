b1afd894d36f0c249ccb92a289f89b97043ca267
Masaya Kataoka <ms.kataoka@gmail.com>
2018-06-13 21:31:13 +0900

    add /current_task_number topic

--- robotx_driver/include/robotx_hardware_interface.h
+++ robotx_driver/include/robotx_hardware_interface.h
@@ -10,6 +10,7 @@
 #include <sensor_msgs/NavSatFix.h>
 #include <std_msgs/Float64MultiArray.h>
 #include <std_msgs/Float64.h>
+#include <std_msgs/UInt8.h>
 
 //headers in boost
 #include <boost/thread.hpp>
@@ -28,7 +29,7 @@ public:
          * @brief action mode
          * @sa robotx_hardware_interface::modes_
          */
-        int mode;
+        int init_mode;
         /**
          * @brief connection timeout [sec] with motor controller
          * 
@@ -65,17 +66,23 @@ public:
          */
         int left_motor_port;
         /**
+         * @brief parameter for team ID (default: OUXT Polaris)
+         * 
+         */
+        std::string team_id;
+        /**
          * @brief set parameters
          * 
          */
         parameters()
         {
-            ros::param::param<int>(ros::this_node::getName()+"/mode", target, ALL);
-            ros::param::param<int>(ros::this_node::getName()+"/mode", mode, REMOTE_OPERATED);
+            ros::param::param<int>(ros::this_node::getName()+"/target", target, ALL);
+            ros::param::param<int>(ros::this_node::getName()+"/init_mode", init_mode, REMOTE_OPERATED);
             ros::param::param<int>(ros::this_node::getName()+"/timeout", timeout, 30);
             ros::param::param<int>(ros::this_node::getName()+"/frequency", frequency, 30);
             ros::param::param<std::string>(ros::this_node::getName()+"/left_motor_ip",left_motor_ip, "127.0.0.1");
             ros::param::param<std::string>(ros::this_node::getName()+"/right_motor_ip",right_motor_ip, "127.0.0.1");
+            ros::param::param<std::string>(ros::this_node::getName()+"/team_id",team_id, "OUXT Polaris");
         };
     };
     robotx_hardware_interface();
@@ -104,6 +111,12 @@ private:
      */
     void fix_callback_(sensor_msgs::NavSatFix msg);
     /**
+     * @brief ROS callback function for /current_task_number topic.
+     * 
+     * @param msg ROS message
+     */
+    void current_task_number_callback_(std_msgs::UInt8 msg);
+    /**
      * @brief send command to motor_driver or simulation.
      * 
      */
@@ -113,23 +126,93 @@ private:
      * 
      */
     void publish_heartbeat_();
+    /**
+     * @brief ROS nodehandle.
+     * 
+     */
     ros::NodeHandle nh_;
+    /**
+     * @brief ROS subscriber for /wam_v/motor_command topic.(message type : std_msgs/Float64MultiArray)
+     * 
+     */
     ros::Subscriber motor_command_sub_;
+    /**
+     * @brief ROS subscriber for /fix topic.(message type : sensot_msgs/NavSatFix)
+     * 
+     */
     ros::Subscriber fix_sub_;
+    /**
+     * @brief ROS subscriber fot /current_task_number topic.(message type : std_msgs/uint8)
+     * 
+     */
+    ros::Subscriber current_task_number_sub_;
+    /**
+     * @brief ROS publisher for /cmd_drive topic.(message type : robotx_msgs/UsvDrive)
+     * 
+     */
     ros::Publisher usv_drive_cmd_pub_;
+    /**
+     * @brief ROS publisher for /left_thruster_position_controller/command topic.(message type : std_msgs/Float64)
+     * 
+     */
     ros::Publisher left_thrust_joint_pub_;
+    /**
+     * @brief ROS publisher for /right_thruster_position_controller/command topic.(message type : std_msgs/Float64)
+     * 
+     */
     ros::Publisher right_thrust_joint_pub_;
+    /**
+     * @brief ROS publisher for ~/heartbeat topic.(message type : robotx_msgs/Heartbeat)
+     * 
+     */
     ros::Publisher heartbeat_pub_;
     sensor_msgs::Joy last_joy_cmd_;
     std_msgs::Float64MultiArray last_motor_cmd_msg_;
     sensor_msgs::NavSatFix last_fix_msg_;
-
+    /**
+     * @brief TCP/IP client for the left motor.
+     * 
+     */
     tcp_client* left_motor_cmd_client_ptr_;
+    /**
+     * @brief TCP/IP client for the right motor.
+     * 
+     */
     tcp_client* right_motor_cmd_client_ptr_;
+    /**
+     * @brief io service fot TCP/IP client
+     * 
+     */
     boost::asio::io_service io_service_;
+    /**
+     * @brief parameters for robotx_hardware_interface class
+     * 
+     */
     const parameters params_;
+    /**
+     * @brief parameter for driving mode.
+     * 
+     */
+    volatile int driving_mode_;
+    /**
+     * @brief parameter for current task number.
+     * 
+     */
+    volatile int current_task_number_;
+    /**
+     * @brief enum parameters describe target.
+     * 
+     */
     enum targets_{ALL=0,SIMULATION=1,HARDWARE=2};
+    /**
+     * @brief enum parameters describe driving mode.
+     * 
+     */
     enum modes_{REMOTE_OPERATED=0,AUTONOMOUS=1,EMERGENCY=2};
+    /**
+     * @brief enum parameters describe joystick controller type.
+     * 
+     */
     enum controllers_{dualshock4=0};
 };
 
--- robotx_driver/src/robotx_hardware_interface.cpp
+++ robotx_driver/src/robotx_hardware_interface.cpp
@@ -5,6 +5,7 @@
 
 robotx_hardware_interface::robotx_hardware_interface() : params_(robotx_hardware_interface::parameters())
 {
+    heartbeat_pub_ = nh_.advertise<robotx_msgs::Heartbeat>("/heartbeat",1);
     if(params_.target == ALL || params_.target == SIMULATION)
     {
         usv_drive_cmd_pub_ = nh_.advertise<robotx_msgs::UsvDrive>("/cmd_drive", 1);
@@ -21,16 +22,25 @@ robotx_hardware_interface::robotx_hardware_interface() : params_(robotx_hardware
         left_motor_cmd_client_ptr_ = new tcp_client(io_service_,params_.left_motor_ip,params_.left_motor_port,params_.timeout);
         right_motor_cmd_client_ptr_ = new tcp_client(io_service_,params_.right_motor_ip,params_.right_motor_port,params_.timeout);
     }
-    heartbeat_pub_ = nh_.advertise<robotx_msgs::Heartbeat>("/heartbeat",1);
+    driving_mode_ = params_.init_mode;
+    current_task_number_ = 0;
     fix_sub_ = nh_.subscribe("/fix", 1, &robotx_hardware_interface::fix_callback_, this);
     motor_command_sub_ = nh_.subscribe("/wam_v/motor_command", 1, &robotx_hardware_interface::motor_command_callback_, this);
     boost::thread send_command_thread(boost::bind(&robotx_hardware_interface::send_command_, this));
+    boost::thread publish_heartbeat_thread(boost::bind(&robotx_hardware_interface::publish_heartbeat_, this));
     send_command_thread.join();
+    publish_heartbeat_thread.join();
 }
 
 robotx_hardware_interface::~robotx_hardware_interface()
 {
 
+}
+
+void robotx_hardware_interface::current_task_number_callback_(std_msgs::UInt8 msg)
+{
+    current_task_number_ = msg.data;
+    return;
 }
 
 void robotx_hardware_interface::fix_callback_(sensor_msgs::NavSatFix msg)
@@ -87,6 +97,26 @@ void robotx_hardware_interface::publish_heartbeat_()
     ros::Rate rate(1);
     while (ros::ok())
     {
+        robotx_msgs::Heartbeat heartbeat_msg;
+        if(last_fix_msg_.latitude > 0)
+            heartbeat_msg.north_or_south = heartbeat_msg.NORTH;
+        else
+            heartbeat_msg.north_or_south = heartbeat_msg.SOUTH;
+        heartbeat_msg.latitude = std::fabs(last_fix_msg_.latitude);
+        if(heartbeat_msg.longitude > 0)
+            heartbeat_msg.east_or_west = heartbeat_msg.EAST;
+        else
+            heartbeat_msg.east_or_west = heartbeat_msg.WEST;
+        heartbeat_msg.longitude = std::fabs(last_fix_msg_.longitude);
+        heartbeat_msg.team_id = params_.team_id;
+        if(driving_mode_ == REMOTE_OPERATED)
+            heartbeat_msg.vehicle_mode = heartbeat_msg.REMOTE_OPERATED;
+        if(driving_mode_ == AUTONOMOUS)
+            heartbeat_msg.vehicle_mode = heartbeat_msg.AUTONOMOUS;
+        if(driving_mode_ == EMERGENCY)
+            heartbeat_msg.vehicle_mode = heartbeat_msg.EMERGENCY;
+        heartbeat_msg.current_task_number = current_task_number_;
+        heartbeat_pub_.publish(heartbeat_msg);
         rate.sleep();
     }
 }
