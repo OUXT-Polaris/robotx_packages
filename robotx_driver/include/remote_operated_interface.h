61e782e08d5088a32a64ca1cd97141d9dd7bec83
Masaya Kataoka <ms.kataoka@gmail.com>
2018-06-14 07:34:50 +0900

    add singal

--- robotx_driver/include/remote_operated_interface.h
+++ robotx_driver/include/remote_operated_interface.h
@@ -4,6 +4,14 @@
 //headers in ROS
 #include <ros/ros.h>
 
+//headers in STL
+#include <functional>
+
+//headers in boost
+#include <boost/system/error_code.hpp>
+#include <boost/signals2.hpp>
+#include <boost/asio.hpp>
+
 /**
  * @brief interface for remote operation
  * 
@@ -28,18 +36,30 @@ public:
             ros::param::param<int>(ros::this_node::getName()+"/controller_type", controller_type, DUALSHOCK4);
         }
     };
+
     /**
      * @brief Construct a new remote operated interface object
      * 
+     * @param set_action_mode_function callback function for set_action_mode
      */
-    remote_operated_interface();
+    remote_operated_interface(std::function<void(int)> set_action_mode_function);
     /**
      * @brief Destroy the remote operated interface object
      * 
      */
     ~remote_operated_interface();
 private:
+    /**
+     * @brief parameters for remote_operated_interface class
+     * 
+     */
     const parameters params_;
+    /**
+     * @brief callback function for set_action_mode
+     * 
+     */
+    std::function<void(int)> set_action_mode_function_;
+    boost::signals2::signal<void (int)> signal_;
 };
 
 #endif  //REMOTE_OPERATED_INTERFACE_H_INCLUDED
--- robotx_driver/src/remote_operated_interface.cpp
+++ robotx_driver/src/remote_operated_interface.cpp
@@ -1,8 +1,11 @@
 //headers in this package
 #include <remote_operated_interface.h>
 
-remote_operated_interface::remote_operated_interface() : params_(remote_operated_interface::parameters())
+remote_operated_interface::remote_operated_interface(std::function<void(int)> set_action_mode_function) 
+    : params_(remote_operated_interface::parameters())
 {
+    set_action_mode_function_ = set_action_mode_function;
+    signal_.connect(set_action_mode_function_);
 }
 
 remote_operated_interface::~remote_operated_interface()
