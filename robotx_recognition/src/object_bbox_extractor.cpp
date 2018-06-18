6b7c81e301302b27d664e4bf96dc135c6f4c188f
Masaya Kataoka <ms.kataoka@gmail.com>
2018-06-19 00:11:05 +0900

    add image_rect raycast

--- robotx_recognition/src/object_bbox_extractor.cpp
+++ robotx_recognition/src/object_bbox_extractor.cpp
@@ -27,7 +27,9 @@ void object_bbox_extractor::image_callback_(const sensor_msgs::ImageConstPtr& ms
     for(int i=0; i<last_bbox_msg_.boxes.size(); i++)
     {
         cv::Rect rect;
-        raycast_to_image(image,last_bbox_msg_.boxes[i],transform_stamped,last_bbox_msg_.header,rect);
+        if(raycast_to_image(image,last_bbox_msg_.boxes[i],transform_stamped,last_bbox_msg_.header,rect))
+        {
+        }
     }
 }
 
@@ -39,15 +41,23 @@ void object_bbox_extractor::euclidean_cluster_callback_(jsk_recognition_msgs::Bo
 bool object_bbox_extractor::raycast_to_image(cv::Mat image, jsk_recognition_msgs::BoundingBox object_bbox, 
     geometry_msgs::TransformStamped transform_stamped, std_msgs::Header header, cv::Rect& image_bbox)
 {
+    image_bbox = cv::Rect();
     double horizontal_fov = params_.horizontal_fov;
     double vertical_fov = params_.horizontal_fov/image.cols*image.rows;
-    std::array<geometry_msgs::PointStamped,8> bbox_points;
-    for(int i=0; i<8; i++)
+    geometry_msgs::PointStamped bbox_center_point;
+    bbox_center_point.header = header;
+    bbox_center_point.point.x = object_bbox.pose.position.x;
+    bbox_center_point.point.y = object_bbox.pose.position.y;
+    bbox_center_point.point.z = object_bbox.pose.position.z;
+    tf2::doTransform(bbox_center_point, bbox_center_point, transform_stamped);
+    std::array<geometry_msgs::PointStamped,4> bbox_points;
+    std::array<cv::Point,4> image_rect_points;
+    for(int i=0; i<4; i++)
     {
-        if(i = 0)
+        if(i == 0)
         {
             bbox_points[i].header = header;
-            bbox_points[i].point.x = object_bbox.pose.position.x + object_bbox.dimensions.x;
+            bbox_points[i].point.x = object_bbox.pose.position.x - object_bbox.dimensions.x;
             bbox_points[i].point.y = object_bbox.pose.position.y + object_bbox.dimensions.y;
             bbox_points[i].point.z = object_bbox.pose.position.z + object_bbox.dimensions.z;
         }
@@ -55,45 +65,17 @@ bool object_bbox_extractor::raycast_to_image(cv::Mat image, jsk_recognition_msgs
         {
             bbox_points[i].header = header;
             bbox_points[i].point.x = object_bbox.pose.position.x - object_bbox.dimensions.x;
-            bbox_points[i].point.y = object_bbox.pose.position.y + object_bbox.dimensions.y;
+            bbox_points[i].point.y = object_bbox.pose.position.y - object_bbox.dimensions.y;
             bbox_points[i].point.z = object_bbox.pose.position.z + object_bbox.dimensions.z;
         }
         if(i == 2)
         {
             bbox_points[i].header = header;
-            bbox_points[i].point.x = object_bbox.pose.position.x + object_bbox.dimensions.x;
-            bbox_points[i].point.y = object_bbox.pose.position.y - object_bbox.dimensions.y;
-            bbox_points[i].point.z = object_bbox.pose.position.z + object_bbox.dimensions.z;
+            bbox_points[i].point.x = object_bbox.pose.position.x - object_bbox.dimensions.x;
+            bbox_points[i].point.y = object_bbox.pose.position.y + object_bbox.dimensions.y;
+            bbox_points[i].point.z = object_bbox.pose.position.z - object_bbox.dimensions.z;
         }
         if(i == 3)
-        {
-            bbox_points[i].header = header;
-            bbox_points[i].point.x = object_bbox.pose.position.x + object_bbox.dimensions.x;
-            bbox_points[i].point.y = object_bbox.pose.position.y + object_bbox.dimensions.y;
-            bbox_points[i].point.z = object_bbox.pose.position.z - object_bbox.dimensions.z;
-        }
-        if(i == 4)
-        {
-            bbox_points[i].header = header;
-            bbox_points[i].point.x = object_bbox.pose.position.x - object_bbox.dimensions.x;
-            bbox_points[i].point.y = object_bbox.pose.position.y - object_bbox.dimensions.y;
-            bbox_points[i].point.z = object_bbox.pose.position.z + object_bbox.dimensions.z;
-        }
-        if(i == 5)
-        {
-            bbox_points[i].header = header;
-            bbox_points[i].point.x = object_bbox.pose.position.x - object_bbox.dimensions.x;
-            bbox_points[i].point.y = object_bbox.pose.position.y + object_bbox.dimensions.y;
-            bbox_points[i].point.z = object_bbox.pose.position.z - object_bbox.dimensions.z;
-        }
-        if(i == 6)
-        {
-            bbox_points[i].header = header;
-            bbox_points[i].point.x = object_bbox.pose.position.x + object_bbox.dimensions.x;
-            bbox_points[i].point.y = object_bbox.pose.position.y - object_bbox.dimensions.y;
-            bbox_points[i].point.z = object_bbox.pose.position.z - object_bbox.dimensions.z;
-        }
-        if(i == 7)
         {
             bbox_points[i].header = header;
             bbox_points[i].point.x = object_bbox.pose.position.x - object_bbox.dimensions.x;
@@ -101,5 +83,39 @@ bool object_bbox_extractor::raycast_to_image(cv::Mat image, jsk_recognition_msgs
             bbox_points[i].point.z = object_bbox.pose.position.z - object_bbox.dimensions.z;
         }
         tf2::doTransform(bbox_points[i], bbox_points[i], transform_stamped);
+        double pitch = std::atan(-bbox_points[i].point.z/bbox_points[i].point.x);
+        double yaw = std::atan(bbox_points[i].point.y/bbox_points[i].point.x);
+        cv::Point image_point;
+        image_point.x = image.cols/2 - yaw/horizontal_fov*image.cols;
+        image_point.y = image.rows/2 - pitch/vertical_fov*image.rows;
+        image_rect_points[i] = image_point;
     }
+    if(image_rect_points[0].y < 0 && image_rect_points[1].y < 0)
+        if(image_rect_points[1].y < 0 && image_rect_points[2].y < 0)
+            return false;
+    if(image_rect_points[0].y > (image.rows-1) && image_rect_points[1].y > (image.rows-1))
+        if(image_rect_points[1].y > (image.rows-1) && image_rect_points[2].y > (image.rows-1))
+            return false;
+    if(image_rect_points[0].x < 0 && image_rect_points[1].x < 0)
+        if(image_rect_points[1].x < 0 && image_rect_points[2].x < 0)
+            return false;
+    if(image_rect_points[0].x > (image.cols-1) && image_rect_points[1].x > (image.cols-1))
+        if(image_rect_points[1].x > (image.cols-1) && image_rect_points[2].x > (image.cols-1))
+            return false;
+    for(int i=0; i<4; i++)
+    {
+        if(image_rect_points[i].x < 0)
+            image_rect_points[i].x = 0;
+        if(image_rect_points[i].x > (image.cols-1))
+            image_rect_points[i].x = image.cols-1;
+        if(image_rect_points[i].y < 0)
+            image_rect_points[i].y = 0;
+        if(image_rect_points[i].y > (image.rows-1))
+            image_rect_points[i].y = image.rows-1;
+    }
+    image_bbox.x = std::min(std::min(image_rect_points[0].x,image_rect_points[1].x),std::min(image_rect_points[2].x,image_rect_points[3].x));
+    image_bbox.y = std::min(std::min(image_rect_points[0].y,image_rect_points[1].y),std::min(image_rect_points[2].y,image_rect_points[3].y));
+    image_bbox.width = std::max(std::max(image_rect_points[0].x,image_rect_points[1].x),std::max(image_rect_points[2].x,image_rect_points[3].x)) - image_bbox.x;
+    image_bbox.height = std::max(std::max(image_rect_points[0].y,image_rect_points[1].y),std::max(image_rect_points[2].y,image_rect_points[3].y)) - image_bbox.y;
+    return true;
 }
