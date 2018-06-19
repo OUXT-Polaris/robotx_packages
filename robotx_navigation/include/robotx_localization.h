f5fb858ea93213f7e14766ea7b5891e4e889e7e7
Masaya Kataoka <ms.kataoka@gmail.com>
2018-06-20 07:21:52 +0900

    remove imu

--- robotx_navigation/include/robotx_localization.h
+++ robotx_navigation/include/robotx_localization.h
@@ -4,8 +4,6 @@
 #include <ros/ros.h>
 #include <sensor_msgs/NavSatFix.h>
 #include <geometry_msgs/Twist.h>
-#include <sensor_msgs/Imu.h>
-#include <Eigen/Core>
 
 class robotx_localization
 {
@@ -19,8 +17,6 @@ public:
         double system_noise_x;
         double system_noise_y;
         double system_noise_theta;
-        double measurement_noise_x;
-        double measurement_noise_y;
         int publish_rate;
         parameters()
         {
@@ -31,8 +27,6 @@ public:
             ros::param::param<double>(ros::this_node::getName()+"/system_noise/x", system_noise_x, 0.05);
             ros::param::param<double>(ros::this_node::getName()+"/system_noise/y", system_noise_y, 0.05);
             ros::param::param<double>(ros::this_node::getName()+"/system_noise/theta", system_noise_theta, 0.05);
-            ros::param::param<double>(ros::this_node::getName()+"/measurement_noise/x", measurement_noise_x, 0.5);
-            ros::param::param<double>(ros::this_node::getName()+"/measurement_noise/y", measurement_noise_y, 0.5);
             ros::param::param<int>(ros::this_node::getName()+"/publish_rate", publish_rate, 100);
         }
     };
@@ -42,23 +36,13 @@ private:
     const parameters params_;
     void fix_callback_(sensor_msgs::NavSatFix msg);
     void twist_callback_(geometry_msgs::Twist msg);
-    void imu_callback_(sensor_msgs::Imu msg);
     ros::Subscriber fix_sub_;
     ros::Subscriber twist_sub_;
-    ros::Subscriber imu_sub_;
     ros::NodeHandle nh_;
-    Eigen::MatrixXd transition_matrix_;
-    Eigen::MatrixXd input_matrix_;
-    Eigen::MatrixXd measurement_matrix_;
-    // noise matrix
-    Eigen::MatrixXd system_noise_matrix_;
-    Eigen::MatrixXd measurement_noise_matrix_;
-    sensor_msgs::NavSatFix init_measurement_;
     sensor_msgs::NavSatFix last_fix_message_;
+    sensor_msgs::NavSatFix init_measurement_;
     geometry_msgs::Twist last_twist_message_;
-    sensor_msgs::Imu last_imu_message_;
     volatile bool fix_recieved_;
     volatile bool twist_received_;
-    volatile bool imu_received_;
 };
 #endif  //ROBOTX_LOCALIZATION_H_INCLUDED
--- robotx_navigation/src/robotx_localization.cpp
+++ robotx_navigation/src/robotx_localization.cpp
@@ -2,26 +2,10 @@
 
 robotx_localization::robotx_localization() : params_()
 {
-    transition_matrix_ = Eigen::MatrixXd::Identity(3,3);
-    input_matrix_ = Eigen::MatrixXd::Zero(3,3);
-    measurement_matrix_ = Eigen::MatrixXd::Identity(2,3);
-    Eigen::VectorXd system_noise_vec = Eigen::VectorXd(3);
-    system_noise_vec[0] = params_.system_noise_x;
-    system_noise_vec[1] = params_.system_noise_y;
-    system_noise_vec[2] = params_.system_noise_theta;
-    system_noise_matrix_ = system_noise_vec.asDiagonal();
-    system_noise_matrix_ = system_noise_matrix_ * system_noise_matrix_;
-    Eigen::VectorXd measurement_noise_vec = Eigen::VectorXd(2);
-    measurement_noise_vec[0] = params_.measurement_noise_x;
-    measurement_noise_vec[1] = params_.measurement_noise_y;
-    measurement_noise_matrix_ = measurement_noise_vec.asDiagonal();
-    measurement_noise_matrix_ = measurement_noise_matrix_ * measurement_noise_matrix_;
     fix_recieved_ = false;
     twist_received_ = false;
-    imu_received_ = false;
     fix_sub_ = nh_.subscribe(params_.fix_topic, 1, &robotx_localization::fix_callback_, this);
     twist_sub_ = nh_.subscribe(params_.twist_topic, 1, &robotx_localization::twist_callback_, this);
-    imu_sub_ = nh_.subscribe(params_.imu_topic, 1, &robotx_localization::imu_callback_, this);
 }
 
 robotx_localization::~robotx_localization()
@@ -43,10 +27,4 @@ void robotx_localization::twist_callback_(geometry_msgs::Twist msg)
 {
     last_twist_message_ = msg;
     twist_received_ = true;
-}
-
-void robotx_localization::imu_callback_(sensor_msgs::Imu msg)
-{
-    last_imu_message_ = msg;
-    imu_received_ = true;
 }
