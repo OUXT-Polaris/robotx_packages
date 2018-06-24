1664c3a6f28a6d23461c2b0a1f6b89b5c9626a8d
Masaya Kataoka <ms.kataoka@gmail.com>
2018-06-24 15:52:10 +0900

    add passthrough_filter nodelet

---
+++ robotx_recognition/src/passthrough_filter_nodelet.cpp
@@ -0,0 +1,20 @@
+#include <passthrough_filter_nodelet.h>
+#include <pluginlib/class_list_macros.h>
+
+namespace robotx_recognition_nodelet
+{
+    passthrough_filter_nodelet::passthrough_filter_nodelet()
+    {
+
+    }
+    passthrough_filter_nodelet::~passthrough_filter_nodelet()
+    {
+
+    }
+    void passthrough_filter_nodelet::onInit()
+    {
+
+    }
+} // robotx_recognition
+
+PLUGINLIB_EXPORT_CLASS(robotx_recognition_nodelet::passthrough_filter_nodelet, nodelet::Nodelet)
