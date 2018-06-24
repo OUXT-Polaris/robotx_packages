0109412cbffefc173f4ca404d8fae6f888ea5f83
Masaya Kataoka <ms.kataoka@gmail.com>
2018-06-24 22:51:12 +0900

    add dependendies

---
+++ robotx_driver/include/ping_sender.h
@@ -0,0 +1,16 @@
+#ifndef PING_SENDER_H_INCLUDED
+#define PING_SENDER_H_INCLUDED
+
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <resolv.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <netinet/ip_icmp.h>
+
+class ping_sender
+{
+
+};
+#endif  //PING_SENDER_H_INCLUDED
---
+++ robotx_driver/src/ping_sender_node.cpp
@@ -0,0 +1,20 @@
+/**
+ * @mainpage ping_sender_node
+ * ROS node for sending ping
+ * @author Masaya Kataoka
+ * @date 2018.06.24
+ * @image html images/logo.jpg
+ */
+
+//headers in this package
+#include <ping_sender.h>
+
+//headers for ros
+#include <ros/ros.h>
+
+int main(int argc, char *argv[])
+{
+  ros::init(argc, argv, "ping_sender_node");
+  ros::spin();
+  return 0;
+}
