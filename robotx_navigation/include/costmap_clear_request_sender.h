52f0f90dfae9c47a2eee5f504866ef1dcff7bf2b
Masaya Kataoka <ms.kataoka@gmail.com>
2018-07-01 14:59:43 +0900

    add .gitignode and costmap_clear_sender source

---
+++ robotx_navigation/include/costmap_clear_request_sender.h
@@ -0,0 +1,10 @@
+#ifndef COSTMAP_CLEAR_REQUEST_SENDER_H_INCLUDED
+#define COSTMAP_CLEAR_REQUEST_SENDER_H_INCLUDED
+
+#include <ros/ros.h>
+
+class costmap_clear_request_sender
+{
+
+};
+#endif  //COSTMAP_CLEAR_REQUEST_SENDER_H_INCLUDED
