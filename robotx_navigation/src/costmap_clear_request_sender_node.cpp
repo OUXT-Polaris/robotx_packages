0b9ab996e2d2f3f860ddbc6c706a70d72ae9d679
Masaya Kataoka <ms.kataoka@gmail.com>
2018-07-01 15:49:45 +0900

    add service client for /move_base/clear_costmap

--- robotx_navigation/src/costmap_clear_request_sender_node.cpp
+++ robotx_navigation/src/costmap_clear_request_sender_node.cpp
@@ -7,6 +7,8 @@
 int main(int argc, char *argv[])
 {
   ros::init(argc, argv, "costmap_clear_request_sender_node");
+  costmap_clear_request_sender sender;
+  sender.run();
   ros::spin();
   return 0;
 }
