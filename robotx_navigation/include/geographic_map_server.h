0f31f6c9611744bb6ca6974910bcfcffcb5d2f37
Masaya Kataoka <ms.kataoka@gmail.com>
2018-07-08 15:21:38 +0900

    update CMakeLists.txt

--- robotx_navigation/include/geographic_map_server.h
+++ robotx_navigation/include/geographic_map_server.h
@@ -7,7 +7,6 @@
 //headers in ROS
 #include <ros/ros.h>
 #include <sensor_msgs/NavSatFix.h>
-#include <geometry_msgs/Vector3Stamped.h>
 #include <yaml-cpp/yaml.h>
 
 //headers in stl
@@ -19,13 +18,20 @@ public:
   struct parameters
   {
     std::string yaml_filepath;
+    std::string origin_fix_topic;
     parameters()
     {
       ros::param::param<std::string>(ros::this_node::getName()+"/yaml_filepath", yaml_filepath, ros::this_node::getName()+"/yaml_filepath");
+      ros::param::param<std::string>(ros::this_node::getName()+"/origin_fix_topic", origin_fix_topic, "/origin/fix");
     }
   };
   geographic_map_server();
   ~geographic_map_server();
 private:
+  const parameters params_;
+  ros::NodeHandle nh_;
+  ros::Subscriber origin_fix_sub_;
+  void origin_fix_callback_(sensor_msgs::NavSatFix msg);
+  robotx_msgs::GeographicMap read_yaml_file();
 };
 #endif //GEOGRAPHIC_MAP_SERVER_H_INCLUDED
--- robotx_navigation/src/geographic_map_server.cpp
+++ robotx_navigation/src/geographic_map_server.cpp
@@ -1,12 +1,34 @@
 //headers in this package
 #include <geographic_map_server.h>
 
-geographic_map_server::geographic_map_server()
+geographic_map_server::geographic_map_server() : params_()
 {
-
+  read_yaml_file();
+  origin_fix_sub_ = nh_.subscribe(params_.origin_fix_topic, 1, &geographic_map_server::origin_fix_callback_, this);
 }
 
 geographic_map_server::~geographic_map_server()
 {
 
+}
+
+void geographic_map_server::origin_fix_callback_(sensor_msgs::NavSatFix msg)
+{
+
+}
+
+robotx_msgs::GeographicMap geographic_map_server::read_yaml_file()
+{
+  robotx_msgs::GeographicMap map;
+  try
+  {
+    YAML::Node map_yaml_data = YAML::LoadFile(params_.yaml_filepath);
+  }
+  catch(YAML::Exception& e)
+  {
+    ROS_ERROR_STREAM(e.what());
+    std::exit(0);
+    return map;
+  }
+  return map;
 }
