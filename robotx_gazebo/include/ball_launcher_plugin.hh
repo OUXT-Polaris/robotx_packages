48b5ef3274a2eba6d58e2c4d1cbda97c7e1bedca
Masaya Kataoka <ms.kataoka@gmail.com>
2018-07-21 18:29:53 +0900

    update navigaion.launch

--- robotx_gazebo/include/ball_launcher_plugin.hh
+++ robotx_gazebo/include/ball_launcher_plugin.hh
@@ -16,47 +16,47 @@
 #include <std_msgs/Empty.h>
 
 namespace gazebo {
-  class ball_launcher_plugin : public ModelPlugin {
-   public:
-    ball_launcher_plugin();
-    virtual ~ball_launcher_plugin();
-    /*! Loads the model in gets dynamic parameters from SDF. */
-    void Load(physics::ModelPtr _parent, sdf::ElementPtr sdf);
-    void OnUpdate(const common::UpdateInfo& _info);
+class ball_launcher_plugin : public ModelPlugin {
+ public:
+  ball_launcher_plugin();
+  virtual ~ball_launcher_plugin();
+  /*! Loads the model in gets dynamic parameters from SDF. */
+  void Load(physics::ModelPtr _parent, sdf::ElementPtr sdf);
+  void OnUpdate(const common::UpdateInfo& _info);
 
-   private:
-    void spawn_ball(int ball_id);
-    void delete_ball();
-    void load_ball_urdf();
-    // launch command subscriber
-    void ball_launcher_callback(std_msgs::Empty msg);
-    ros::Subscriber launch_sub_;
+ private:
+  void spawn_ball(int ball_id);
+  void delete_ball();
+  void load_ball_urdf();
+  // launch command subscriber
+  void ball_launcher_callback(std_msgs::Empty msg);
+  ros::Subscriber launch_sub_;
 
-    // model parameters
-    std::string ball_sdf_path_;
-    physics::LinkPtr ball_launcher_link_ptr_;
-    gazebo::math::Pose ball_launcher_link_pose_;
-    physics::ModelPtr model_ptr_;
-    physics::ModelPtr ball_model_ptr_;
-    physics::WorldPtr world_ptr_;
-    ros::Duration ball_lifetime_;
-    event::ConnectionPtr update_connection_;
-    int num_balls_;
-    int ball_remains_;
-    int count_;
-    // volatile bool ball_exist_;
+  // model parameters
+  std::string ball_sdf_path_;
+  physics::LinkPtr ball_launcher_link_ptr_;
+  gazebo::math::Pose ball_launcher_link_pose_;
+  physics::ModelPtr model_ptr_;
+  physics::ModelPtr ball_model_ptr_;
+  physics::WorldPtr world_ptr_;
+  ros::Duration ball_lifetime_;
+  event::ConnectionPtr update_connection_;
+  int num_balls_;
+  int ball_remains_;
+  int count_;
+  // volatile bool ball_exist_;
 
-    // ROS nodehandle
-    ros::NodeHandle nh_;
-    // ROS Publisher
-    ros::Publisher ball_launcher_status_pub_;
-    // spanm model service client
-    ros::ServiceClient spawn_client_;
-    ros::Time last_spawm_time_;
-    // delete model service client
-    // ros::ServiceClient delete_client_;
-    std::string ball_urdf_str_;
-  };  // class ball_launcher_plugin
+  // ROS nodehandle
+  ros::NodeHandle nh_;
+  // ROS Publisher
+  ros::Publisher ball_launcher_status_pub_;
+  // spanm model service client
+  ros::ServiceClient spawn_client_;
+  ros::Time last_spawm_time_;
+  // delete model service client
+  // ros::ServiceClient delete_client_;
+  std::string ball_urdf_str_;
+};  // class ball_launcher_plugin
 }  // namespace gazebo
 
 #endif  // BALL_LAUNCHER_PLUGIN_H
--- robotx_gazebo/src/ball_launcher_plugin.cc
+++ robotx_gazebo/src/ball_launcher_plugin.cc
@@ -14,17 +14,14 @@ using namespace gazebo;
 
 ball_launcher_plugin::ball_launcher_plugin() {}
 ball_launcher_plugin::~ball_launcher_plugin() {}
-void ball_launcher_plugin::Load(physics::ModelPtr _parent, sdf::ElementPtr sdf)
-{
+void ball_launcher_plugin::Load(physics::ModelPtr _parent, sdf::ElementPtr sdf) {
   // ball_exist_ = false;
   count_ = 0;
   // Read ball sdf file path
-  ball_sdf_path_ =
-      ros::package::getPath("robotx_gazebo") + "/models/ball/ball.urdf";
+  ball_sdf_path_ = ros::package::getPath("robotx_gazebo") + "/models/ball/ball.urdf";
   std::string default_ball_launcher_link_name = "ball_launcher_pitch_link";
   std::string ball_launcher_link_name;
-  LoadParams(sdf, "target_link", ball_launcher_link_name,
-             default_ball_launcher_link_name);
+  LoadParams(sdf, "target_link", ball_launcher_link_name, default_ball_launcher_link_name);
   LoadParams(sdf, "num_balls", num_balls_, 5);
   ball_remains_ = num_balls_;
   model_ptr_ = _parent;
@@ -35,33 +32,28 @@ void ball_launcher_plugin::Load(physics::ModelPtr _parent, sdf::ElementPtr sdf)
   LoadParams(sdf, "lifetime", lifetime, 5.0);
   ball_lifetime_ = ros::Duration(lifetime);
 
-  ball_launcher_status_pub_ = nh_.advertise<robotx_msgs::BallLauncherStatus>(
-      "/ball_launcher/status", 10);
+  ball_launcher_status_pub_ = nh_.advertise<robotx_msgs::BallLauncherStatus>("/ball_launcher/status", 10);
 
   load_ball_urdf();
-  spawn_client_ =
-      nh_.serviceClient<gazebo_msgs::SpawnModel>("/gazebo/spawn_urdf_model");
+  spawn_client_ = nh_.serviceClient<gazebo_msgs::SpawnModel>("/gazebo/spawn_urdf_model");
   // delete_client_ =
   // nh_.serviceClient<gazebo_msgs::DeleteModel>("/gazebo/delete_model");
 
-  this->update_connection_ = event::Events::ConnectWorldUpdateBegin(
-      boost::bind(&ball_launcher_plugin::OnUpdate, this, _1));
+  this->update_connection_ =
+      event::Events::ConnectWorldUpdateBegin(boost::bind(&ball_launcher_plugin::OnUpdate, this, _1));
   launch_sub_ =
-      nh_.subscribe("/ball_launcher/launch_ball", 1,
-                    &ball_launcher_plugin::ball_launcher_callback, this);
+      nh_.subscribe("/ball_launcher/launch_ball", 1, &ball_launcher_plugin::ball_launcher_callback, this);
   return;
 }
 
-void ball_launcher_plugin::ball_launcher_callback(std_msgs::Empty /*msg*/)
-{
+void ball_launcher_plugin::ball_launcher_callback(std_msgs::Empty /*msg*/) {
   if (ball_remains_ > 0) {
     spawn_ball(num_balls_ - ball_remains_);
     ball_remains_ = ball_remains_ - 1;
   }
 }
 
-void ball_launcher_plugin::OnUpdate(const common::UpdateInfo& /*_info*/)
-{
+void ball_launcher_plugin::OnUpdate(const common::UpdateInfo& /*_info*/) {
   ball_launcher_link_pose_ = ball_launcher_link_ptr_->GetWorldPose();
   count_ = count_ + 1;
   if (count_ % 100 == 0) {
@@ -73,16 +65,14 @@ void ball_launcher_plugin::OnUpdate(const common::UpdateInfo& /*_info*/)
   }
 }
 
-void ball_launcher_plugin::load_ball_urdf()
-{
+void ball_launcher_plugin::load_ball_urdf() {
   ball_urdf_str_ = "";
   std::ifstream ifs(ball_sdf_path_);
   if (ifs.fail()) {
     ROS_ERROR_STREAM("failed to find " << ball_sdf_path_);
     std::exit(0);
   }
-  std::string str((std::istreambuf_iterator<char>(ifs)),
-                  std::istreambuf_iterator<char>());
+  std::string str((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
   ball_urdf_str_ = str;
   return;
 }
@@ -103,8 +93,7 @@ void ball_launcher_plugin::delete_ball()
 }
 */
 
-void ball_launcher_plugin::spawn_ball(int ball_id)
-{
+void ball_launcher_plugin::spawn_ball(int ball_id) {
   // see also
   // http://docs.ros.org/kinetic/api/gazebo_msgs/html/srv/SpawnModel.html
   gazebo_msgs::SpawnModel msg;
@@ -122,8 +111,7 @@ void ball_launcher_plugin::spawn_ball(int ball_id)
   if (spawn_client_.call(msg)) {
     last_spawm_time_ = ros::Time::now();
     // ball_exist_ = true;
-  }
-  else {
+  } else {
     ROS_ERROR_STREAM("failed to spawn ball");
   }
   return;
