4573809a22845c60f46fe341be44fb752e5eeb1e
Masaya Kataoka <ms.kataoka@gmail.com>
2018-07-22 11:09:33 +0900

    add cnn_result_visualizer node

---
+++ robotx_tools/include/cnn_result_visualizer.h
@@ -0,0 +1,32 @@
+#ifndef CNN_RESULT_VISUALIZER_H_INCLUDED
+#define CNN_RESULT_VISUALIZER_H_INCLUDED
+
+// headers in this package
+#include <robotx_msgs/ObjectRegionOfInterestArray.h>
+
+// headers in jsk_rviz_plugins
+#include <jsk_rviz_plugins/PictogramArray.h>
+
+// headers in ROS
+#include <ros/ros.h>
+
+class cnn_result_visualizer {
+ public:
+  struct parameters {
+    std::string object_roi_topic;
+    parameters() {
+      ros::param::param<std::string>(ros::this_node::getName() + "/object_roi_topic", object_roi_topic,
+                                     ros::this_node::getName() + "/object_roi");
+    }
+  };
+  cnn_result_visualizer();
+  ~cnn_result_visualizer();
+
+ private:
+  const parameters params_;
+  ros::NodeHandle nh_;
+  ros::Subscriber object_roi_sub_;
+  ros::Publisher pictgram_array_pub_;
+  void object_roi_callback(const robotx_msgs::ObjectRegionOfInterestArray::ConstPtr msg);
+};
+#endif  // CNN_RESULT_VISUALIZER_H_INCLUDED
---
+++ robotx_tools/src/cnn_result_visualizer.cpp
@@ -0,0 +1,8 @@
+#include <cnn_result_visualizer.h>
+
+cnn_result_visualizer::cnn_result_visualizer() : params_() {}
+
+cnn_result_visualizer::~cnn_result_visualizer() {}
+
+void cnn_result_visualizer::object_roi_callback(
+    const robotx_msgs::ObjectRegionOfInterestArray::ConstPtr msg) {}
