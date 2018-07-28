6014e90be5bc2f91a8f8cd4d13ef9661bda12437
Masaya Kataoka <ms.kataoka@gmail.com>
2018-07-28 11:08:01 +0900

    add ball model push function

--- robotx_gazebo/include/ball_launcher_plugin.hh
+++ robotx_gazebo/include/ball_launcher_plugin.hh
@@ -10,10 +10,13 @@
 // #include <gazebo/physics/Model.hh>
 
 // headers in ROS
+#include <geometry_msgs/Quaternion.h>
 #include <robotx_msgs/BallLauncherStatus.h>
 #include <ros/package.h>
 #include <ros/ros.h>
 #include <std_msgs/Empty.h>
+#include <tf/LinearMath/Matrix3x3.h>
+#include <tf/LinearMath/Quaternion.h>
 
 namespace gazebo {
 class ball_launcher_plugin : public ModelPlugin {
@@ -22,7 +25,7 @@ class ball_launcher_plugin : public ModelPlugin {
   virtual ~ball_launcher_plugin();
   /*! Loads the model in gets dynamic parameters from SDF. */
   void Load(physics::ModelPtr _parent, sdf::ElementPtr sdf);
-  void OnUpdate(const common::UpdateInfo& _info);
+  void OnUpdate(const common::UpdateInfo &_info);
 
  private:
   void spawn_ball(int ball_id);
@@ -30,6 +33,7 @@ class ball_launcher_plugin : public ModelPlugin {
   void load_ball_urdf();
   // launch command subscriber
   void ball_launcher_callback(std_msgs::Empty msg);
+  void get_rpy(geometry_msgs::Quaternion &q, double &roll, double &pitch, double &yaw);
   ros::Subscriber launch_sub_;
 
   // model parameters
@@ -44,6 +48,8 @@ class ball_launcher_plugin : public ModelPlugin {
   int num_balls_;
   int ball_remains_;
   int count_;
+  // initial speed of the ball
+  double ball_v_init_;
   // volatile bool ball_exist_;
 
   // ROS nodehandle
@@ -53,8 +59,6 @@ class ball_launcher_plugin : public ModelPlugin {
   // spanm model service client
   ros::ServiceClient spawn_client_;
   ros::Time last_spawm_time_;
-  // delete model service client
-  // ros::ServiceClient delete_client_;
   std::string ball_urdf_str_;
 };  // class ball_launcher_plugin
 }  // namespace gazebo
--- robotx_gazebo/src/ball_launcher_plugin.cc
+++ robotx_gazebo/src/ball_launcher_plugin.cc
@@ -10,6 +10,9 @@
 // headers in STL
 #include <fstream>
 
+// headers in Eigen
+#include <Eigen/Core>
+
 using namespace gazebo;
 
 ball_launcher_plugin::ball_launcher_plugin() {}
@@ -23,6 +26,7 @@ void ball_launcher_plugin::Load(physics::ModelPtr _parent, sdf::ElementPtr sdf) 
   std::string ball_launcher_link_name;
   LoadParams(sdf, "target_link", ball_launcher_link_name, default_ball_launcher_link_name);
   LoadParams(sdf, "num_balls", num_balls_, 5);
+  LoadParams(sdf, "ball_v_init", ball_v_init_, 3.0);
   ball_remains_ = num_balls_;
   model_ptr_ = _parent;
   ball_launcher_link_ptr_ = model_ptr_->GetLink(ball_launcher_link_name);
@@ -43,6 +47,7 @@ void ball_launcher_plugin::Load(physics::ModelPtr _parent, sdf::ElementPtr sdf) 
       event::Events::ConnectWorldUpdateBegin(boost::bind(&ball_launcher_plugin::OnUpdate, this, _1));
   launch_sub_ =
       nh_.subscribe("/ball_launcher/launch_ball", 1, &ball_launcher_plugin::ball_launcher_callback, this);
+  world_ptr_ = model_ptr_->GetWorld();
   return;
 }
 
@@ -53,7 +58,7 @@ void ball_launcher_plugin::ball_launcher_callback(std_msgs::Empty /*msg*/) {
   }
 }
 
-void ball_launcher_plugin::OnUpdate(const common::UpdateInfo& /*_info*/) {
+void ball_launcher_plugin::OnUpdate(const common::UpdateInfo & /*_info*/) {
   ball_launcher_link_pose_ = ball_launcher_link_ptr_->GetWorldPose();
   count_ = count_ + 1;
   if (count_ % 100 == 0) {
@@ -77,22 +82,6 @@ void ball_launcher_plugin::load_ball_urdf() {
   return;
 }
 
-/*
-void ball_launcher_plugin::delete_ball()
-{
-  gazebo_msgs::DeleteModel msg;
-  msg.request.model_name = "ball";
-  if (delete_client_.call(msg)) {
-    ROS_INFO_STREAM(msg.response.status_message);
-    ball_exist_ = false;
-  }
-  else {
-    ROS_ERROR_STREAM("failed to delete ball");
-  }
-  return;
-}
-*/
-
 void ball_launcher_plugin::spawn_ball(int ball_id) {
   // see also
   // http://docs.ros.org/kinetic/api/gazebo_msgs/html/srv/SpawnModel.html
@@ -100,20 +89,37 @@ void ball_launcher_plugin::spawn_ball(int ball_id) {
   msg.request.model_name = "ball_" + std::to_string(ball_id);
   msg.request.model_xml = ball_urdf_str_;
   msg.request.reference_frame = "";
-  msg.request.initial_pose.position.x = 0;
-  msg.request.initial_pose.position.y = 0;
-  msg.request.initial_pose.position.z = 0;
-  msg.request.initial_pose.orientation.x = 0;
-  msg.request.initial_pose.orientation.y = 0;
-  msg.request.initial_pose.orientation.z = 0;
-  msg.request.initial_pose.orientation.w = 1;
+  msg.request.initial_pose.position.x = ball_launcher_link_pose_.pos.x;
+  msg.request.initial_pose.position.y = ball_launcher_link_pose_.pos.y;
+  msg.request.initial_pose.position.z = ball_launcher_link_pose_.pos.z;
+  msg.request.initial_pose.orientation.x = ball_launcher_link_pose_.rot.x;
+  msg.request.initial_pose.orientation.y = ball_launcher_link_pose_.rot.y;
+  msg.request.initial_pose.orientation.z = ball_launcher_link_pose_.rot.z;
+  msg.request.initial_pose.orientation.w = ball_launcher_link_pose_.rot.w;
   msg.request.robot_namespace = "ball_" + std::to_string(ball_id);
   if (spawn_client_.call(msg)) {
     last_spawm_time_ = ros::Time::now();
-    // ball_exist_ = true;
+    physics::ModelPtr ball_ptr = world_ptr_->GetModel(msg.request.robot_namespace);
+    double roll, pitch, yaw;
+    Eigen::MatrixXd Rx(3, 3);
+    Eigen::MatrixXd Ry(3, 3);
+    Eigen::MatrixXd Rz(3, 3);
+    get_rpy(msg.request.initial_pose.orientation, roll, pitch, yaw);
+    Rx << std::cos(roll), -std::sin(roll), 0, std::sin(roll), std::cos(roll), 0, 0, 0, 1;
+    Ry << std::cos(pitch), 0, std::sin(pitch), 0, 1, 0, -std::sin(pitch), 0, std::cos(pitch);
+    Rz << 1, 0, 0, 0, std::cos(yaw), -std::sin(yaw), 0, std::sin(yaw), std::cos(yaw);
+    Eigen::MatrixXd v_init(3, 1);
+    v_init << ball_v_init_, 0, 0;
+    v_init = Rx * Ry * Rz * v_init;
+    ball_ptr->SetLinearVel(ignition::math::Vector3d(v_init(0, 0), v_init(1, 0), v_init(2, 0)));
   } else {
     ROS_ERROR_STREAM("failed to spawn ball");
   }
   return;
+}
+
+void ball_launcher_plugin::get_rpy(geometry_msgs::Quaternion &q, double &roll, double &pitch, double &yaw) {
+  tf::Quaternion quat(q.x, q.y, q.z, q.w);
+  tf::Matrix3x3(quat).getRPY(roll, pitch, yaw);
 }
 GZ_REGISTER_MODEL_PLUGIN(ball_launcher_plugin)
