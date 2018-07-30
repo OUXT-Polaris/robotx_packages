e7892634687ff8b5f9ab5fa58b9d754d28400ec0
Masaya Kataoka <ms.kataoka@gmail.com>
2018-07-30 21:19:57 +0900

    add message_filters

--- robotx_navigation/include/ndt_mapping.h
+++ robotx_navigation/include/ndt_mapping.h
@@ -1,15 +1,22 @@
 #ifndef NDT_MAPPING_H_INCLUDED
 #define NDT_MAPPING_H_INCLUDED
 
+// headers in ROS
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+#include <nav_msgs/Odometry.h>
 #include <ros/ros.h>
+#include <sensor_msgs/PointCloud2.h>
 
 class ndt_mapping {
  public:
   struct parameters {
     std::string pointcloud_topic;
+    std::string odom_topic;
     parameters() {
       ros::param::param<std::string>(ros::this_node::getName() + "/pointcloud_topic", pointcloud_topic,
                                      ros::this_node::getName() + "/input_pointcloud");
+      ros::param::param<std::string>(ros::this_node::getName() + "/odom_topic", odom_topic, "/odom");
     }
   };
   ndt_mapping();
@@ -18,5 +25,10 @@ class ndt_mapping {
  private:
   const parameters params_;
   ros::NodeHandle nh_;
+  message_filters::Subscriber<nav_msgs::Odometry> odom_sub_;
+  message_filters::Subscriber<sensor_msgs::PointCloud2> pointcloud_sub_;
+  message_filters::TimeSynchronizer<nav_msgs::Odometry, sensor_msgs::PointCloud2> sync_;
+  void callback_(const nav_msgs::OdometryConstPtr& odom_msg,
+                 const sensor_msgs::PointCloud2ConstPtr& pointcloud_msg);
 };
 #endif  // NDT_MAPPING_H_INCLUDED
--- robotx_navigation/src/ndt_mapping.cpp
+++ robotx_navigation/src/ndt_mapping.cpp
@@ -1,5 +1,10 @@
 #include <ndt_mapping.h>
 
-ndt_mapping::ndt_mapping() : params_() {}
+ndt_mapping::ndt_mapping() : params_(), sync_(odom_sub_, pointcloud_sub_, 10) {
+  sync_.registerCallback(boost::bind(&ndt_mapping::callback_, this, _1, _2));
+}
 
 ndt_mapping::~ndt_mapping() {}
+
+void ndt_mapping::callback_(const nav_msgs::OdometryConstPtr& odom_msg,
+                            const sensor_msgs::PointCloud2ConstPtr& pointcloud_msg) {}
