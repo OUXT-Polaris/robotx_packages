f6d216869167d4105b4540bb9a1eb67ebab0984f
Masaya Kataoka <ms.kataoka@gmail.com>
2018-08-05 11:21:06 +0900

    add ${Boost_LIBRARIES}

--- robotx_tools/include/tcp_server.h
+++ robotx_tools/include/tcp_server.h
@@ -1,4 +1,11 @@
 #ifndef TCP_SERVER_H_INCLUDED
 #define TCP_SERVER_H_INCLUDED
 
+#include <boost/asio.hpp>
+
+class tcp_server {
+ public:
+  tcp_server();
+  ~tcp_server();
+};
 #endif  // TCP_SERVER_H_INCLUDED
---
+++ robotx_tools/src/tcp_server.cpp
@@ -0,0 +1,5 @@
+#include <tcp_server.h>
+
+tcp_server::tcp_server() {}
+
+tcp_server::~tcp_server() {}
