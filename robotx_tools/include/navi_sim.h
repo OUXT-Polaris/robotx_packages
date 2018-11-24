5c6a7938d44221f597a51acd47637558068b8013
Masaya Kataoka <ms.kataoka@gmail.com>
2018-11-24 23:57:02 +0900

    enable publish fix message

--- robotx_tools/include/navi_sim.h
+++ robotx_tools/include/navi_sim.h
@@ -5,9 +5,22 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/Pose2D.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <geometry_msgs/QuaternionStamped.h>
+#include <sensor_msgs/NavSatFix.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf/tf.h>
 
 //headers in this package
 #include <UTM.h>
+
+//headers in STL
+#include <mutex>
+
+//headers in boost
+#include <boost/optional.hpp>
 
 class navi_sim
 {
@@ -16,13 +29,26 @@ public:
     ~navi_sim();
     void run();
 private:
+    std::mutex mtx_;
     geometry_msgs::Twist twist_cmd_;
     ros::NodeHandle nh_;
     ros::NodeHandle pnh_;
-    geometry_msgs::Pose2D current_pose_;
+    ros::Subscriber twist_cmd_sub_;
+    ros::Subscriber init_pose_sub_;
+    ros::Publisher fix_pub_;
+    ros::Publisher true_course_pub_;
+    boost::optional<geometry_msgs::Pose2D> current_pose_;
+    std::string gps_frame_;
     std::string world_frame_;
+    std::string fix_topic_;
+    std::string true_course_topic_;
+    bool southhemi_;
+    int utm_zone_;
     double update_rate_;
     void update_();
-    void twist_callback_(const geometry_msgs::Twist::ConstPtr msg);
+    void cmd_vel_callback(const geometry_msgs::Twist::ConstPtr msg);
+    void init_pose_callback_(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr msg);
+    tf2_ros::Buffer tf_buffer_;
+    tf2_ros::TransformListener tf_listener_;
 };
 #endif  //NAVI_SIM_H_INCLUDED
--- robotx_tools/src/navi_sim.cpp
+++ robotx_tools/src/navi_sim.cpp
@@ -1,11 +1,20 @@
 #include <navi_sim.h>
 
-navi_sim::navi_sim()
+navi_sim::navi_sim() : tf_listener_(tf_buffer_)
 {
     nh_ = ros::NodeHandle();
     pnh_ = ros::NodeHandle("~");
+    pnh_.param<int>("utm_zone", utm_zone_, 0);
     pnh_.param<double>("update_rate", update_rate_, 10);
+    pnh_.param<bool>("southhemi", southhemi_, false);
+    pnh_.param<std::string>("gps_frame", gps_frame_, "gps");
     pnh_.param<std::string>("world_frame", world_frame_, "world");
+    pnh_.param<std::string>("fix_topic", fix_topic_, "/fix");
+    pnh_.param<std::string>("true_course_topic", true_course_topic_, "/true_course");
+    fix_pub_ = nh_.advertise<sensor_msgs::NavSatFix>(fix_topic_,1);
+    true_course_pub_ = nh_.advertise<geometry_msgs::QuaternionStamped>(true_course_topic_,1);
+    twist_cmd_sub_ = nh_.subscribe("/cmd_vel",1,&navi_sim::cmd_vel_callback,this);
+    init_pose_sub_ = nh_.subscribe("/initialpose",1,&navi_sim::init_pose_callback_,this);
 }
 
 navi_sim::~navi_sim()
@@ -13,9 +22,49 @@ navi_sim::~navi_sim()
     
 }
 
-void navi_sim::twist_callback_(const geometry_msgs::Twist::ConstPtr msg)
+void navi_sim::cmd_vel_callback(const geometry_msgs::Twist::ConstPtr msg)
 {
+    mtx_.lock();
     twist_cmd_ = *msg;
+    mtx_.unlock();
+    return;
+}
+
+void navi_sim::init_pose_callback_(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr msg)
+{   
+    mtx_.lock();
+    geometry_msgs::PoseStamped pose_msg;
+    pose_msg.header = msg->header;
+    pose_msg.pose = msg->pose.pose;
+    geometry_msgs::PoseStamped transformed_pose;
+    geometry_msgs::TransformStamped transform_stamped;
+    if(msg->header.frame_id != world_frame_)
+    {
+        try
+        {
+            transform_stamped = tf_buffer_.lookupTransform(world_frame_, msg->header.frame_id, ros::Time(0));
+        }
+        catch (tf2::TransformException &ex)
+        {
+            ROS_WARN("%s",ex.what());
+            return;
+        }
+        tf2::doTransform(pose_msg,transformed_pose,transform_stamped);
+    }
+    else
+    {
+        transformed_pose = pose_msg;
+    }
+    tf::Quaternion quat(transformed_pose.pose.orientation.x,transformed_pose.pose.orientation.y,
+        transformed_pose.pose.orientation.z,transformed_pose.pose.orientation.w);
+    double r,p,y;
+    tf::Matrix3x3(quat).getRPY(r, p, y);
+    geometry_msgs::Pose2D pose;
+    pose.theta = y;
+    pose.x = transformed_pose.pose.position.x;
+    pose.y = transformed_pose.pose.position.y;
+    current_pose_ = pose;
+    mtx_.unlock();
     return;
 }
 
@@ -24,6 +73,25 @@ void navi_sim::update_()
     ros::Rate rate(update_rate_);
     while(ros::ok())
     {
+        mtx_.lock();
+        double lat,lon;
+        if(current_pose_)
+        {
+            UTMXYToLatLon(current_pose_->x, current_pose_->y, utm_zone_, southhemi_, lat, lon);
+            sensor_msgs::NavSatFix fix_msg;
+            fix_msg.header.frame_id = gps_frame_;
+            fix_msg.header.stamp = ros::Time::now();
+            fix_msg.latitude = lat;
+            fix_msg.longitude = lon;
+            fix_pub_.publish(fix_msg);
+            geometry_msgs::QuaternionStamped quat_msg;
+            quat_msg.header.frame_id = gps_frame_;
+            quat_msg.header.stamp = ros::Time::now();
+            tf::Quaternion quat = tf::createQuaternionFromRPY(0,0,current_pose_->theta);
+            quaternionTFToMsg(quat, quat_msg.quaternion);
+            true_course_pub_.publish(quat_msg);
+        }
+        mtx_.unlock();
         rate.sleep();
     }
     return;
@@ -31,5 +99,6 @@ void navi_sim::update_()
 
 void navi_sim::run()
 {
+    boost::thread update_thread_(&navi_sim::update_,this);
     return;
 }
