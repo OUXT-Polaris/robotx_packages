008d2c8026957e9e6fba176c33c1710b77c4c780
Masaya Kataoka <ms.kataoka@gmail.com>
2018-12-01 11:32:12 +0900

    add combobox

--- robotx_tools/src/waypoint_clicker_plugin.cpp
+++ robotx_tools/src/waypoint_clicker_plugin.cpp
@@ -1,4 +1,4 @@
-
+#include <QComboBox>
 #include <QPainter>
 #include <QVBoxLayout>
 
@@ -6,9 +6,20 @@
 
 namespace robotx_tools
 {
-WaypointClickerPanel::WaypointClickerPanel(QWidget *parent) : rviz::Panel(parent)
-{
-}
+    WaypointClickerPanel::WaypointClickerPanel(QWidget *parent) : rviz::Panel(parent)
+    {
+        QStringList commands = 
+            { "mission_start", "demonstrate_navigation_and_control","go_to_start_position", "entrance_gates", 
+            "scan_the_code", "exit_gates", "avoid_obstacles","find_totems", "identify_symbols_and_dock",
+            "return_to_home","mission_complete"};
+        QVBoxLayout *layout = new QVBoxLayout(this);
+        layout->addWidget(new QLabel("Task"));
+        QComboBox* combo = new QComboBox();
+        combo->addItems(commands);
+        layout->addWidget(combo);
+        setLayout(layout);
+        //connect( combo, &QComboBox::currentTextChanged, this, &MainWindow::commandChanged);
+    }
 
 }  // robotx_tools
 
